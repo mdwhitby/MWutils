ca3cfb08dd0b3f7bddf80cd91b8946bb143698d6
mdwhitby <github@keepeverycog.com>
2022-09-20 21:21:23 -0400

    rearrange; tabnum

---
+++ R/MarkdownUtils.R
@@ -0,0 +1,33 @@
+
+#' custom knit function
+#'
+#' @param input
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+knit_with_date <- function(input,
+                           output_dir=paste0(here::here(), '/Reports'),
+                           ...) {
+  rmarkdown::render(
+    input,
+    output_file = paste0(
+      xfun::sans_ext(input), '-', format(Sys.Date(), "%Y%m%d"), '.'
+    ),
+    output_dir =output_dir,
+    envir = globalenv()
+  )
+}
+
+
+#' auto Table Numbers in Markdown
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tabN <- function() {
+  tNum <<- tNum+1
+}
--- R/RandoF.R
+++ R/RandoF.R
@@ -1,26 +1,73 @@
 
-#' custom knit function
+
+#' Send an email (use phone number email to receieve a text)
 #'
-#' @param input
+#' @param message
+#' @param mail_from
+#' @param mail_rcpt
+#' @param smtp_server
+#' @param use_ssl
+#' @param Username
+#' @param Password
 #' @param ...
 #'
 #' @return
 #' @export
 #'
 #' @examples
-knit_with_date <- function(input,
-                           output_dir=paste0(here::here(), '/Reports'),
-                           ...) {
-  rmarkdown::render(
-    input,
-    output_file = paste0(
-      xfun::sans_ext(input), '-', format(Sys.Date(), "%Y%m%d"), '.'
-    ),
-    output_dir =output_dir,
-    envir = globalenv()
+#'
+notifyMe <- function(message = 'msg from R',
+                     mail_from = "rmail@keepeverycog.com",
+                     mail_rcpt = '6099230973@vtext.com',
+                     smtp_server = "mail.keepeverycog.com:465",
+                     use_ssl = "force",
+                     Username = "rmail@keepeverycog.com",
+                     Password = keyring::key_get('RMail_KEC', username = 'rmail@keepeverycog.com'),
+                     ...
+) {
+
+  curl::send_mail(
+    mail_from = mail_from,
+    mail_rcpt = mail_rcpt,
+    message = message,
+    smtp_server=smtp_server,
+    use_ssl=use_ssl,
+    Username = Username,
+    Password = Password,
+    ...
   )
+
 }
 
+
+#' get ESRI arcgis online REST token
+#'
+#' @param username string of username
+#' @param secret string of password
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getESRItoken <- function(username, secret) {
+
+  token <-
+    httr::POST(
+      url = "https://www.arcgis.com/sharing/rest/generateToken",
+      encode = "form",
+      body = list(
+        username = username,
+        password = secret,
+        f = "json",
+        client= "referer",
+        referer="batcon.maps.arcgis.com",
+        expiration = 60
+      )
+    )
+
+  return(httr::content(token)$token)
+
+}
 
 #' Prepare for parallel processing Quickly
 #'
@@ -30,6 +77,7 @@ knit_with_date <- function(input,
 #' @export
 #'
 #' @examples
+#'
 initCL <- function(pcnt=0.75){
   library(parallel)
   library(pbapply)
